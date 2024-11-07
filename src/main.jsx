70f99feaa33acdfa7bfe8a762012deea169f8f6e
Carson Chang <cadchang@gimchang30@gmail.com>
2024-11-06 20:51:32 -0500

    uhh

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // Ensure this import is present
+import './index.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/kbbq-menu/', // Replace with your repository name
+  base: '/KBBQ-Menu/', // Replace with your repository name
   plugins: [react()],
 })
